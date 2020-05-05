71cd16c893f07a098ef262829630dab52c11908d
Pricej541 <japrice@mavs.coloradomesa.edu>
2020-05-04 21:39:49 -0600

    2nd attempt at a proper push...

--- ComputerSelectionService/ComputerSelectionService/ProfessionalForm1.vb
+++ ComputerSelectionService/ComputerSelectionService/ProfessionalForm1.vb
@@ -1,11 +1,31 @@
-﻿Public Class ProfessionalForm1
-    Private Sub ProfessionalForm1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.FormBorderStyle = Windows.Forms.FormBorderStyle.None
-        Me.WindowState = FormWindowState.Maximized
+﻿Imports ComputerSelectionService.ProfessionalVariables
+
+Public Class ProfessionalForm1
+    Private Sub Lessthan500_Click(sender As Object, e As EventArgs) Handles Lessthan500.Click
+        PriceRange = "less than 500"
+        ProfessionalForm2.Show()
+        Me.Hide()
     End Sub
 
-    Private Sub HomeButton_Click(sender As Object, e As EventArgs) Handles HomeButton.Click
+    Private Sub FivetoThousand_Click(sender As Object, e As EventArgs) Handles FivetoThousand.Click
+        PriceRange = "500 to 1000"
+        ProfessionalForm2.Show()
         Me.Hide()
-        StartupPage.Show()
+    End Sub
+
+    Private Sub OverAThousand_Click(sender As Object, e As EventArgs) Handles OverAThousand.Click
+        PriceRange = "Over 1000"
+        ProfessionalForm2.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim frm = New StartupPage               '' Change the class name if necessary
+        frm.Show()
+        Me.Close()
     End Sub
 End Class
